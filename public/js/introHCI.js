c773f03479173eb4390618bb01fabd970e2998f7
LuckyNguyen <lkn007@ucsd.edu>
2016-02-10 00:56:32 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,19 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, clickProj);
+	console.log("project/" + idNumber);
 	console.log("User clicked on project " + idNumber);
+}
+
+
+function clickProj(result){
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' + 
+	'<img src"' + result['image'] + '"class="img detailsImage">' + 
+	'<p><small>'+ result['title'] + '</small></p>' + 
+	'<p><small>' + result['date'] + '</small></p></a>';
+	$("#project" + result['id']).find(".details").html(projectHTML + result['summary']);
 }
 
 /*
@@ -35,4 +47,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette/", clickColors);
+}
+
+function clickColors(result){
+	console.log(result);
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
